f1ffda3452622caefc962814ed0df62ae3354789
Yang-Yefeng <18B904013@stu.hit.edu.cn>
2021-11-13 15:00:59 +0800

    Update samplingmap.py

--- Map/Continuous/samplingmap.py
+++ Map/Continuous/samplingmap.py
@@ -1,3 +1,5 @@
+import copy
+
 import numpy as np
 import cv2 as cv
 import os
@@ -38,7 +40,7 @@ class samplingmap:
             # self.terminal = np.array([x_size - 0.5, y_size - 0.5]) if terminal is None else np.array(terminal)
             self.terminal = [x_size - 0.5, y_size - 0.5] if terminal is None else terminal
             self.obs = obs
-            self.obs_num = len(obs)
+            self.obs_num = 0 if obs is None else len(obs)
         else:
             pass
         self.image = np.zeros([self.width, self.height, 3], np.uint8)
@@ -60,6 +62,14 @@ class samplingmap:
         return min(point) < 0 or point[0] >= self.x_size or point[1] >= self.y_size
 
     @staticmethod
+    def cross_product(vec1, vec2):
+        return vec1[0] * vec2[1] - vec2[0] * vec1[1]
+
+    @staticmethod
+    def dis_two_points(point1, point2):
+        return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+
+    @staticmethod
     def point_is_in_circle(center, r, point):
         sub = [center[i] - point[i] for i in [0, 1]]
         return np.linalg.norm(sub) <= r
@@ -79,6 +89,160 @@ class samplingmap:
         [x, y] = list(np.dot(trans, sub))
         return (x / long) ** 2 + (y / short) ** 2 <= 1
 
+    def point_is_in_poly(self, center, r, points, point):
+        if self.point_is_in_circle(center, r, point) is False:
+            return False
+        '''若在多边形对应的外接圆内，再进行下一步判断'''
+        l_pts = len(points)
+        res = False
+        j = l_pts - 1
+        for i in range(l_pts):
+            if ((points[i][1] > point[1]) != (points[j][1] > point[1])) and \
+                    (point[0] < (points[j][0] - points[i][0]) * (point[1] - points[i][1]) / (points[j][1] - points[i][1]) + points[i][0]):
+                res = not res
+            j = i
+        if res is True:
+            return True
+
+    def line_is_in_circle(self, center, r, point1, point2):
+        return self.line_is_in_ellipse(r, r, 0, center, point1, point2)
+
+    def line_is_in_ellipse(self, long, short, rotate_angle, center, point1, point2):
+        """
+        :param long:                长轴
+        :param short:               短轴
+        :param rotate_angle:        椭圆自身的旋转角度
+        :param center:              中心点
+        :param point1:              待测点1
+        :param point2:              待测点2
+        :return:                    bool
+        """
+        pt1 = [point1[i] - center[i] for i in [0, 1]]
+        pt2 = [point2[j] - center[j] for j in [0, 1]]
+
+        pptt1 = [pt1[0]*cosd(-rotate_angle) - pt1[1]*sind(-rotate_angle), pt1[0]*sind(-rotate_angle) + pt1[1]*cosd(-rotate_angle)]
+        pptt2 = [pt2[0]*cosd(-rotate_angle) - pt2[1]*sind(-rotate_angle), pt2[0]*sind(-rotate_angle) + pt2[1]*cosd(-rotate_angle)]
+        c = math.sqrt(long ** 2 - short ** 2)
+        '''求椭圆焦点坐标'''
+        ellipse_1 = [-c, 0]
+        ellipse_2 = [c, 0]
+        '''求椭圆焦点坐标'''
+
+        '''求直线方程'''
+        if pptt1[0] == pptt2[0]:
+            k = np.inf
+            b = pptt1[0]
+        else:
+            k = (pptt2[1] - pptt1[1]) / (pptt2[0] - pptt1[0])
+            b = pptt1[1] - k * pptt1[0]
+        '''求直线方程'''
+
+        if k == np.inf:     # 如果是垂直线
+            if self.point_is_in_ellipse(long, short, 0, [0, 0], pptt1):
+                return True
+            if self.point_is_in_ellipse(long, short, 0, [0, 0], pptt2):
+                return True
+            if math.fabs(pptt1[0]) <= long and pptt1[1] * pptt2[1] < 0:
+                return True
+            return False
+        elif k == 0:        # 如果是水平线
+            if self.point_is_in_ellipse(long, short, 0, [0, 0], pptt1):
+                return True
+            if self.point_is_in_ellipse(long, short, 0, [0, 0], pptt2):
+                return True
+            if math.fabs(b) <= short and pptt1[0] * pptt2[0] < 0:
+                return True
+            return False
+        else:               # 其他
+            if self.point_is_in_ellipse(long, short, 0, [0, 0], pptt1):
+                return True
+            if self.point_is_in_ellipse(long, short, 0, [0, 0], pptt2):
+                return True
+            '''椭圆关于直线的对称点'''
+            ellipse_2_mirror = [((1 - k ** 2) * ellipse_2[0] + 2 * k * ellipse_2[1] - 2 * k * b) / (k ** 2 + 1),
+                                (2 * k * ellipse_2[0] + (k ** 2 - 1) * ellipse_2[1] + 2 * b) / (k ** 2 + 1)]
+            '''椭圆两焦点到该直线距离和的最小值'''
+            dis = self.dis_two_points(ellipse_1, ellipse_2_mirror)
+            if dis > 2 * long:
+                return False
+            else:
+                mirror_x = (ellipse_2_mirror[0] + ellipse_2[0]) / 2
+                if (mirror_x >= max(pptt1[0], pptt2[0])) or (mirror_x <= min(pptt1[0], pptt2[0])):
+                    return False
+                else:
+                    return True
+
+    def line_is_in_poly(self, center, r, points, point1, point2):
+        if self.point_is_in_poly(center, r, points, point1):
+            print('Something wrong happened...')
+            return True
+        if self.point_is_in_poly(center, r, points, point2):
+            print('Something wrong happened...')
+            return True
+        length = len(points)
+        # c = point1
+        # d = point2
+        for i in range(length):
+            a = points[i % length]
+            b = points[(i + 1) % length]
+            c = copy.deepcopy(point1)
+            d = copy.deepcopy(point2)
+            '''通过坐标变换将a点变到原点'''
+            b = [b[i] - a[i] for i in [0, 1]]
+            c = [c[i] - a[i] for i in [0, 1]]
+            d = [d[i] - a[i] for i in [0, 1]]
+            a = [a[i] - a[i] for i in [0, 1]]
+            '''通过坐标变换将a点变到原点'''
+            '''通过坐标旋转将b点变到与X重合'''
+            l_ab = self.dis_two_points(a, b)     # length of ab
+            cos = b[0] / l_ab
+            sin = b[1] / l_ab
+            bb = [cos * b[0] + sin*b[1], -sin*b[0] + cos*b[1]]
+            cc = [cos * c[0] + sin*c[1], -sin*c[0] + cos*c[1]]
+            dd = [cos * d[0] + sin*d[1], -sin*d[0] + cos*d[1]]
+            '''通过坐标旋转将b点变到与X重合'''
+            if cc[1] * dd[1] > 0:
+                '''如果变换后的cd纵坐标在x轴的同侧'''
+                # return False
+                continue
+            else:
+                '''如果变换后的cd纵坐标在x轴的异侧(包括X轴)'''
+                if cc[0] == dd[0]:
+                    '''k == inf'''
+                    if  min(bb) <= cc[0] <= max(bb):
+                        return True
+                    else:
+                        continue
+                else:
+                    '''k != inf'''
+                    k_cd = (dd[1] - cc[1]) / (dd[0] - cc[0])
+                    b_cd = cc[1] - k_cd * cc[0]
+                    x_cross = -b_cd / k_cd
+                    if min(bb) <= x_cross <= max(bb):
+                        return True
+                    else:
+                        continue
+        return False
+
+    def line_is_in_obs(self, point1, point2) -> bool:
+        for _obs in self.obs:
+            if _obs[0] == 'circle':
+                if self.line_is_in_circle(_obs[2], _obs[1][0], point1, point2):
+                    # print('line_is_in_circle', point1, point2)
+                    print('line_is_in_circle')
+                    return True
+            elif _obs[0] == 'ellipse':
+                if self.line_is_in_ellipse(_obs[1][0], _obs[1][1], _obs[1][2], _obs[2], point1, point2):
+                    # print('line_is_in_ellipse', point1, point2)
+                    print('line_is_in_ellipse')
+                    return True
+            else:
+                if self.line_is_in_poly([_obs[1][i] for i in [0, 1]], _obs[1][2], _obs[2], point1, point2):
+                    # print('line_is_in_poly', point1, point2)
+                    print('line_is_in_poly')
+                    return True
+        return False
+
     def point_is_in_obs(self, point: list) -> bool:
         for _obs in self.obs:
             if _obs[0] == 'circle':
@@ -87,28 +251,15 @@ class samplingmap:
                 else:
                     continue
             elif _obs[0] == 'ellipse':
-                if not self.point_is_in_ellipse(_obs[1][0], _obs[1][1], _obs[1][2], _obs[2], point):
-                    continue
+                if self.point_is_in_ellipse(_obs[1][0], _obs[1][1], _obs[1][2], _obs[2], point):
+                    return True
                 else:
-                    return True
-            else:
-                '''第一步先判断点是否在多边形的外接圆内，为了加速'''
-                center = [_obs[1][i] for i in [0, 1]]
-                r = _obs[1][2]
-                if self.point_is_in_circle(center, r, point) is False:
                     continue
-                '''若在多边形对应的外接圆内，再进行下一步判断'''
-                pts = _obs[2]
-                l_pts = len(_obs[2])
-                res = False
-                j = l_pts - 1
-                for i in range(l_pts):
-                    if ((pts[i][1] > point[1]) != (pts[j][1] > point[1])) and  \
-                            (point[0] < (pts[j][0] - pts[i][0]) * (point[1] - pts[i][1]) / (pts[j][1] - pts[i][1]) + pts[i][0]):
-                        res = not res
-                    j = i
-                if res is True:
+            else:
+                if self.point_is_in_poly([_obs[1][i] for i in [0, 1]], _obs[1][2], _obs[2], point):
                     return True
+                else:
+                    continue
         return False
 
     def pixel2dis(self, point):
@@ -151,6 +302,9 @@ class samplingmap:
         cv.circle(self.image, self.dis2pixel(self.terminal), 5, Color().Blue, -1)
 
     def map_draw_obs(self):
+        if self.obs is None:
+            print('No obstacles!!')
+            return
         for [name, constraints, pts] in self.obs:   # [name, [], [pt1, pt2, pt3]]
             if name == 'circle':
                 cv.circle(self.image, self.dis2pixel(pts), self.length2pixel(constraints[0]), Color().DarkGray, -1)
@@ -163,7 +317,6 @@ class samplingmap:
                            endAngle=360.,
                            color=Color().DarkGray,
                            thickness=-1)
-                # cv.ellipse(self.image, self.dis2pixel(pts), (constraints[0], constraints[1]), constraints[2], 0, 360, -1)
             else:
                 cv.fillConvexPoly(self.image, points=np.array([list(self.dis2pixel(pt)) for pt in pts]), color=Color().DarkGray)
 
@@ -174,15 +327,18 @@ class samplingmap:
         cv.imshow(self.name4image, self.image)
         cv.waitKey(0)
 
-    def path_draw(self, path):
+    def path_draw(self, path, name, color):
         pt1 = path.pop()
         pt1_int = self.dis2pixel(pt1)
         while path:
             pt2 = path.pop()
             pt2_int = self.dis2pixel(pt2)
-            cv.line(self.image, pt1_int, pt2_int, Color().Orange, 2)
+            cv.line(self.image, pt1_int, pt2_int, color, 2)
+            # cv.imshow(self.name4image, self.image)
+            # cv.waitKey(0)
             pt1 = pt2
             pt1_int = self.dis2pixel(pt1)
         cv.imshow(self.name4image, self.image)
+        cv.imwrite('../../../somefigures/figure/' + name, self.image)
         cv.waitKey(0)
         cv.destroyAllWindows()
