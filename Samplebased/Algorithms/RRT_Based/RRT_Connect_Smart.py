3a68fd144c5e0c83f2e9f6418ee22be4db4b7b45
Yang-Yefeng <18B904013@stu.hit.edu.cn>
2021-11-16 23:28:10 +0800

    Update RRT_Connect_Smart.py

--- Samplebased/Algorithms/RRT_Based/RRT_Connect_Smart.py
+++ Samplebased/Algorithms/RRT_Based/RRT_Connect_Smart.py
@@ -10,8 +10,6 @@ class RRT_Connect_Smart(RRT_Connect):
         super(RRT_Connect_Smart, self).__init__(width, height, x_size, y_size, image_name, start, terminal, obs, map_file)
 
     def smart_optimize(self):
-        self.path_find()
-        self.path_draw(self.waypoint, 'rrt_connect_smart.png', Color().Orange)
         s = tuple(self.terminal)
         sterminal = tuple(self.terminal)
         while True:
@@ -20,9 +18,8 @@ class RRT_Connect_Smart(RRT_Connect):
                 self.parent[sterminal] = ss
                 s = ss
             else:
-                # print('拐了', s, ss, sterminal)
+                print('A new line:', s, ss, sterminal)
                 sterminal = copy.deepcopy(s)
-                # break
             if s == self.parent[s]:  # tuple(self.start)
                 break
 
@@ -47,8 +44,10 @@ class RRT_Connect_Smart(RRT_Connect):
                 cv.waitKey(1)
             video_record.write(self.image)
             if self.two_tree_connect():
+                self.path_find()
                 print('Successful, start to optimize...')
                 print('Press any key to continue...')
+                self.path_draw(self.waypoint, 'rrt_connect_smart.png', Color().Orange)
                 self.smart_optimize()
                 self.path_find2()
                 self.path_draw(self.waypoint, 'rrt_connect_smart.png', Color().Red)
@@ -70,31 +69,57 @@ class RRT_Connect_Smart(RRT_Connect):
 
 
 if __name__ == '__main__':
-    # obstacles = [['triangle',  [1.5, 5],   [1.0, 60.0, 0.0]],
-    #              ['rectangle', [3, 3.5],   [2.0, 5.0, 0.]],
-    #              ['rectangle', [4, 1],     [1.5, 5.0, -20.]],
-    #              ['pentagon',  [7, 8.5],   [1.0, 180.0]],
-    #              ['hexagon',   [8.0, 2],   [1.0, 30.0]],
-    #              ['triangle',  [8.0, 5],   [1.0, 40.0, 20.0]],
-    #              ['hexagon',   [5.5, 2],   [0.5, 0.0]],
-    #              ['circle',    [6, 6],     [1.0]],
-    #              ['ellipse',   [3, 8],     [2.6, 0.6, -20.0]],
-    #              ['pentagon',  [3.4, 6.0], [0.6, 50]],
-    #              ['pentagon',  [8.7, 6.4], [0.8, 108]],
-    #              ['ellipse',   [1.0, 2.5], [0.8, 0.6, 60.0]],
-    #              ['pentagon',  [6.5, 4.2], [0.46, 25.0]]]
-    obstacles = [
-        ['rectangle', [7, 8],   [2.0, 74.0, 0.]],
-        ['rectangle', [4, 6],   [3.0, 5.0, 0.]],
-        ['rectangle', [3, 3.5], [2.0, 5.0, 0.]]
+    obstacles1 = [
+        ['triangle',  [1.5, 5],   [1.0, 60.0, 0.0]],
+        ['rectangle', [3, 3.5],   [2.0, 5.0, 0.]],
+        ['rectangle', [4, 1],     [1.5, 5.0, -20.]],
+        ['pentagon',  [7, 8.5],   [1.0, 180.0]],
+        ['hexagon',   [8.0, 2],   [1.0, 30.0]],
+        ['triangle',  [8.0, 5],   [1.0, 40.0, 20.0]],
+        ['hexagon',   [5.5, 2],   [0.5, 0.0]],
+        ['circle',    [6, 6],     [1.0]],
+        ['ellipse',   [3, 8],     [2.6, 0.6, -20.0]],
+        ['pentagon',  [3.4, 6.0], [0.6, 50]],
+        ['pentagon',  [8.7, 6.4], [0.8, 108]],
+        ['ellipse',   [1.0, 2.5], [0.8, 0.6, 60.0]],
+        ['pentagon',  [6.5, 4.2], [0.46, 25.0]]]
+    obstacles2 = [
+        ['rectangle', [7, 8],   [2.0, 85.0, 0.]],
+        ['rectangle', [4, 6],   [3.2, 5.0, 0.]],
+        ['rectangle', [3, 3.5], [2.0, 5.0, 0.]],
+        ['rectangle', [5, 2], [2.0, 85.0, 0.]]
     ]
+    obstacles3 = [
+        ['circle', [6, 6], [1.2]],
+        ['circle', [3, 3], [1.0]],
+        ['circle', [6, 2], [1.0]],
+        ['circle', [1, 9], [1.0]],
+        ['circle', [4, 7], [1.0]],
+        ['circle', [9, 7], [0.4]],
+        ['circle', [3, 5], [1.0]],
+        ['circle', [8, 5], [0.5]],
+        ['circle', [1, 6], [1.0]],
+        ['circle', [8, 1], [0.5]],
+        ['circle', [8, 8], [0.5]],
+        ['circle', [7.5, 7], [0.5]],
+    ]
+    obstacles4 = [
+        ['ellipse', [6, 6], [1.6, 0.6, -20.0]],
+        ['ellipse', [3, 3], [1.6, 1.0, -15.0]],
+        ['ellipse', [6, 2], [1.6, 0.4, -10.0]],
+        ['ellipse', [5, 9], [3.6, 0.8, -5.0]],
+        ['ellipse', [4, 7], [1.6, 0.2, 0.0]],
+        ['ellipse', [3.5, 5], [3.6, 0.4, -20.0]],
+        ['ellipse', [8, 4.6], [3.6, 0.4, 90.0]],
+    ]
+    obstacles = obstacles4
     obstacles = obstacle(obstacles).get_obs()
     rrt_connect = RRT_Connect_Smart(width=400,
                                     height=400,
                                     x_size=10,
                                     y_size=10,
                                     image_name='samplingmap',
-                                    start=[4.5, 8.5],     # 0.5, 0.5  4.5, 8.5
+                                    start=[0.5, 0.5],     # 0.5, 0.5  4.5, 8.5
                                     terminal=[9.5, 9.5],
                                     obs=obstacles,
                                     map_file=None)
