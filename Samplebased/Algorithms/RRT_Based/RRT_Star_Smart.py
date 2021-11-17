73e64fe1e140d6799284019400fb881622a1eba8
Yang-Yefeng <18B904013@stu.hit.edu.cn>
2021-11-17 18:46:52 +0800

    Update RRT_Star_Smart.py

--- Samplebased/Algorithms/RRT_Based/RRT_Star_Smart.py
+++ Samplebased/Algorithms/RRT_Based/RRT_Star_Smart.py
@@ -1,7 +1,9 @@
-import copy
-from Map.Continuous.obstacle import obstacle
-import cv2 as cv
-from Map.Color.Color import Color
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) +
+                "/../../../../PathPlanningAlgorithms/")
+
+from Samplebased.Algorithms.RRT_Based import *
 from Samplebased.Algorithms.RRT_Based.RRT_Star import RRT_Star
 
 
